d35fd86561a58cac415ae16f46a2626815b182cc
KokilaSuresh0331 <160492094+KokilaSuresh0331@users.noreply.github.com>
2024-04-21 13:19:26 +0530

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -18,57 +18,54 @@ public static void main(String[] args) {
 	}
 	if (solveSudoku(a)) {
         System.out.println("Sudoku solved successfully:");
-        printBoard(a);
+        Display(a);
     } else {
         System.out.println("No solution exists for the given Sudoku problem.");
     }
 }
 private static boolean solveSudoku(int [][]board) {
-	int n=board.length;
-	 for (int row = 0; row < n; row++) {
-         for (int col = 0; col < n; col++) {
-             if (board[row][col] == 0) {
-                 for (int num = 1; num <= n; num++) {
-                     if (isValid(board, row, col, num)) {
-                         board[row][col] = num;
-
-                         if (solveSudoku(board)) {
-                             return true;
-                         } else {
-                             board[row][col] = 0;
-                         }
-                     }
-                 }
-                 return false;
-             }
-         }
-     }
-
-	return true;
-
-}
-private static boolean isValid(int[][] board, int row, int col, int num) {
-	 int n = board.length;
-	for (int i = 0; i < n; i++) {
-         if (board[row][i] == num || board[i][col] == num || board[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
-             return false;
-         }
-     }
-	
-	int sqrtN = (int) Math.sqrt(n);
-    int startRow = row - row % sqrtN;
-    int startCol = col - col % sqrtN;
-
-    for (int i = 0; i < sqrtN; i++) {
-        for (int j = 0; j < sqrtN; j++) {
-            if (board[i + startRow][j + startCol] == num) {
+	int n = board.length;
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+            if (board[row][col] == 0) {
+                for (int num = 1; num <= n; num++) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+                        if (solveSudoku(board)) {
+                            return true;
+                        } else {
+                            board[row][col] = 0;
+                        }
+                    }
+                }
                 return false;
             }
         }
     }
-	 return true;
+    return true;
+}
+
+private static boolean isValid(int[][] board, int row, int col, int num) {
+	 int n = board.length;
+	 int sqrtN = (int) Math.sqrt(n);
+	 for (int i = 0; i < n; i++) {
+         if (board[row][i] == num || board[i][col] == num) {
+             return false;
+         }
+     }
+	
+	 int startRow = row - row % sqrtN;
+     int startCol = col - col % sqrtN;
+     for (int i = 0; i < sqrtN; i++) {
+         for (int j = 0; j < sqrtN; j++) {
+             if (board[i + startRow][j + startCol] == num) {
+                 return false;
+             }
+         }
+     }
+     return true;
  }
-private static void printBoard(int[][] board) {
+private static void Display(int[][] board) {
 	System.out.print("Display Array:\n");
 	for(int i=0;i<board.length;i++) {
 		for(int j=0;j<board.length;j++) {
@@ -77,9 +74,7 @@ private static void printBoard(int[][] board) {
 		}
 		System.out.println("|");
 		
-	}
-	
-	
+	}		
 }
 }
 
